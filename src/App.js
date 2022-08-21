5369d5d3531766547e202adb2823b4699225f65f
exmacorp <jorgeluisalvaing@gmail.com>
2022-08-20 22:58:07 -0500

    work navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Nabvar from './components/Nabvar';
+
+function App() {
+  return (
+    <>
+      <Nabvar />
+    </>
+  );
+}
+
+export default App;
